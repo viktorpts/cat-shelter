2c5950b26a29d086af6650780659611ac7b607d9
Viktor Kostadinov <viktor.pts@abv.bg>
2024-05-16 20:53:18 +0300

    Add initial files

---
+++ src/handlers/static.js
@@ -0,0 +1,32 @@
+const { readFile } = require('../util');
+
+function staticFileHandler(req, res) {
+    if (req.url.endsWith('.css')) {
+        // handle stylesheet
+        const data = readFile(req.url);
+        res.writeHead(200, [
+            'Content-Type', 'text/css'
+        ]);
+        res.write(data);
+        res.end();
+
+        return true;
+    } else if (req.url.endsWith('.ico')) {
+        // handle favicon
+        const data = readFile(req.url);
+        console.log(data);
+        res.writeHead(200, [
+            'Content-Type', 'image/svg+xml'
+        ]);
+        res.write(data);
+        res.end();
+
+        return true;
+    }
+
+    return false;
+}
+
+module.exports = {
+    staticFileHandler
+};
